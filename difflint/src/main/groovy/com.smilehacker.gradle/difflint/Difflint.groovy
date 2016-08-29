e638ac0fcf2e74b9425ee38c55a4ce923a097b60
Sai <zhouquanbest@gmail.com>
2016-08-29 20:05:34 +0800

    init

---
+++ difflint/src/main/groovy/com.smilehacker.gradle/difflint/Difflint.groovy
@@ -0,0 +1,12 @@
+package com.smilehacker.gradle.difflint
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class Difflint implements Plugin<Project> {
+    void apply(Project project) {
+        project.task('testTask') << {
+            println "Hello gradle plugin"
+        }
+    }
+}
