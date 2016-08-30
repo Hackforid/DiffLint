d4f229c21bd6e61c012bf1e1d94ca06a7461ddc7
Sai <zhouquanbest@gmail.com>
2016-08-30 10:59:04 +0800

    update

--- difflint/src/main/groovy/com.smilehacker.gradle/difflint/Difflint.groovy
+++ difflint/src/main/groovy/com.smilehacker.gradle/difflint/Difflint.groovy
@@ -7,6 +7,8 @@ public class Difflint implements Plugin<Project> {
     void apply(Project project) {
         project.task('testTask') << {
             println "Hello gradle plugin"
+            GitHelper helper = new GitHelper()
+            helper.init()
         }
     }
 }
--- difflint/src/main/groovy/com.smilehacker.gradle/difflint/GitHelper.java
+++ difflint/src/main/groovy/com.smilehacker.gradle/difflint/GitHelper.java
@@ -1,5 +1,8 @@
 package com.smilehacker.gradle.difflint;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.Status;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
@@ -10,10 +13,35 @@ import java.io.IOException;
  */
 public class GitHelper {
     public void init() {
+        Repository  repo = null;
         try {
-            Repository repo = new FileRepositoryBuilder().readEnvironment().build();
+            repo = new FileRepositoryBuilder()
+                    //.setGitDir(new File("./"))
+                    .readEnvironment()
+                    .findGitDir()
+                    .build();
         } catch (IOException e) {
             e.printStackTrace();
+            return;
         }
+
+        Git git = new Git(repo);
+        try {
+            Status status = git.status().call();
+            System.out.println("Added: " + status.getAdded());
+            System.out.println("Changed: " + status.getChanged());
+            System.out.println("Conflicting: " + status.getConflicting());
+            System.out.println("ConflictingStageState: " + status.getConflictingStageState());
+            System.out.println("IgnoredNotInIndex: " + status.getIgnoredNotInIndex());
+            System.out.println("Missing: " + status.getMissing());
+            System.out.println("Modified: " + status.getModified());
+            System.out.println("Removed: " + status.getRemoved());
+            System.out.println("Untracked: " + status.getUntracked());
+            System.out.println("UntrackedFolders: " + status.getUntrackedFolders());
+
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+        }
+
     }
 }
